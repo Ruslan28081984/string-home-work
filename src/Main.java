c4ea3456301d3b994a96c2dc197885034693442c
user1 <89201828020@mail.ru>
2022-07-29 17:32:53 +0300

    Сделал задачи 1, 2 и 3.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1and2();
+        task1and3();
+
     }
-}
+        public static  void task1and2() {
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = " Ivanov";
+            String fullName = String.join(" ", lastName, firstName, middleName);
+            System.out.println(" ФИО сотрудника - " + fullName);
+            System.out.println(fullName.toUpperCase());
+        }
+            public static  void task1and3(){
+            String firstName1 = "Семён ";
+            String middleName1 = "Семёнович ";
+            String lastName1 = " Иванов ";
+            String fullName1 = lastName1 + firstName1 + middleName1;
+            fullName1 = fullName1.replace('ё', 'е');
+            System.out.println( fullName1);
+        }
+    }
